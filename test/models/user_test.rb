4c0f52d41e778cb6501e2abd69f4662b223ee95a
Johannes / universa1 <jhedtrich@gmail.com>
2014-10-09 12:29:11 +0200

    add tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  test "create habtm" do
+    user = User.create name: "test"
+    user.transacts.create name: "transact"
+    user.reload
+    assert user.transacts.any?, "no transacts added"
+  end
+
+  test "build/save habtm" do
+    user = User.create name: "test"
+    transact = user.transacts.build name: "transact"
+    transact.save
+    user.reload
+    assert user.transacts.any?, "no transacts added"
+  end
+
+  test "build/save habtm with manual user added" do
+    user = User.create name: "test"
+    transact = user.transacts.build name: "transact", users: [user]
+    assert transact.new_record?, "assert transact still new"
+    transact.save
+    user.reload
+    assert user.transacts.any?, "no transacts added"
+  end
   # test "the truth" do
   #   assert true
   # end
